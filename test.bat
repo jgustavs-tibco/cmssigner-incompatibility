38acbb9dd9fed632237e8e1b7026af1a336c9758
Erik Brandin <ebrandin@tibco.com>
2022-10-24 14:35:23 +0200

    Removed sub directory.

--- test.bat
+++ test.bat
@@ -1,22 +1,22 @@
 @echo off
 
 echo Test create signature in .Net Framework. Verify in .Net Framework using elliptic curve.
-.\SignedCmsIncompatibility\SignedCmsIncompatibility\bin\Debug\net48\SignedCmsIncompatibility.exe CreateECCertificate
-.\SignedCmsIncompatibility\SignedCmsIncompatibility\bin\Debug\net48\SignedCmsIncompatibility.exe CreateSignature
-.\SignedCmsIncompatibility\SignedCmsIncompatibility\bin\Debug\net48\SignedCmsIncompatibility.exe VerifySignature
+.\SignedCmsIncompatibility\bin\Debug\net48\SignedCmsIncompatibility.exe CreateECCertificate
+.\SignedCmsIncompatibility\bin\Debug\net48\SignedCmsIncompatibility.exe CreateSignature
+.\SignedCmsIncompatibility\bin\Debug\net48\SignedCmsIncompatibility.exe VerifySignature
 
 echo Test create signature in .Net Core. Verify in .Net Core using elliptic curve.
-.\SignedCmsIncompatibility\SignedCmsIncompatibility\bin\Debug\net6.0\SignedCmsIncompatibility.exe CreateECCertificate
-.\SignedCmsIncompatibility\SignedCmsIncompatibility\bin\Debug\net6.0\SignedCmsIncompatibility.exe CreateSignature
-.\SignedCmsIncompatibility\SignedCmsIncompatibility\bin\Debug\net6.0\SignedCmsIncompatibility.exe VerifySignature
+.\SignedCmsIncompatibility\bin\Debug\net6.0\SignedCmsIncompatibility.exe CreateECCertificate
+.\SignedCmsIncompatibility\bin\Debug\net6.0\SignedCmsIncompatibility.exe CreateSignature
+.\SignedCmsIncompatibility\bin\Debug\net6.0\SignedCmsIncompatibility.exe VerifySignature
 
 echo Test create signature in .Net Framework. Verify in .Net Core using RSA.
-.\SignedCmsIncompatibility\SignedCmsIncompatibility\bin\Debug\net48\SignedCmsIncompatibility.exe CreateRSACertificate
-.\SignedCmsIncompatibility\SignedCmsIncompatibility\bin\Debug\net48\SignedCmsIncompatibility.exe CreateSignature
-.\SignedCmsIncompatibility\SignedCmsIncompatibility\bin\Debug\net6.0\SignedCmsIncompatibility.exe VerifySignature
+.\SignedCmsIncompatibility\bin\Debug\net48\SignedCmsIncompatibility.exe CreateRSACertificate
+.\SignedCmsIncompatibility\bin\Debug\net48\SignedCmsIncompatibility.exe CreateSignature
+.\SignedCmsIncompatibility\bin\Debug\net6.0\SignedCmsIncompatibility.exe VerifySignature
 
 echo Test create signature in .Net Framework. Verify in .Net Core using elliptic curve.
-.\SignedCmsIncompatibility\SignedCmsIncompatibility\bin\Debug\net48\SignedCmsIncompatibility.exe CreateECCertificate
-.\SignedCmsIncompatibility\SignedCmsIncompatibility\bin\Debug\net48\SignedCmsIncompatibility.exe CreateSignature
-.\SignedCmsIncompatibility\SignedCmsIncompatibility\bin\Debug\net6.0\SignedCmsIncompatibility.exe VerifySignature
+.\SignedCmsIncompatibility\bin\Debug\net48\SignedCmsIncompatibility.exe CreateECCertificate
+.\SignedCmsIncompatibility\bin\Debug\net48\SignedCmsIncompatibility.exe CreateSignature
+.\SignedCmsIncompatibility\bin\Debug\net6.0\SignedCmsIncompatibility.exe VerifySignature
 
